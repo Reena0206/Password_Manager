6939f89bf7a7fba388310c00ca86739049cef591
Reena0206 <155416498+Reena0206@users.noreply.github.com>
2025-08-03 12:39:13 +0530

    Add files via upload

---
+++ password_with_searching.py
@@ -0,0 +1,119 @@
+from tkinter import *
+from tkinter import messagebox
+from gen_password import gen_pass
+import json
+#-----------------------------Save Passwords----------------------------------#
+
+
+
+root = Tk()
+root.config(padx=20,pady=20)
+
+can = Canvas(height=500,width=500)
+logo = PhotoImage(file='pass.png')
+
+can.create_image(250,250,image=logo)
+# can.pack()
+can.grid(row=0,column=1)
+
+web = Label(root,text="Website: ",font=('Arial',10,'bold'))
+web.grid(row=1,column=0,pady=2)
+em = Label(root,text="Email/Username: ",font=('Arial',10,'bold'))
+em.grid(row=2,column=0,pady=2)
+password = Label(root,text="Password: ",font=('Arial',10,'bold'))
+password.grid(row=3,column=0,pady=2)
+
+
+
+web_entry = Entry(root,width=87)
+web_entry.grid(row=1,column=1,columnspan=2,sticky='w')
+web_entry.focus()
+em_entry = Entry(root,width=87)
+em_entry.grid(row=2,column=1,columnspan=2,sticky='w')
+em_entry.insert(0,'reena@gmail.com')
+password_entry = Entry(root,width=87)
+password_entry.grid(row=3,column=1,sticky='w')
+
+
+#-------------Add Button---------------#
+def add_data():
+    web = web_entry.get().capitalize()
+    em = em_entry.get()
+    password = password_entry.get()
+    # print(f"{web}|{em}|{password}")
+    # data = f"{web}|{em}|{password}"
+    new_data = {
+        web:{
+            'email':em,
+            'password':password,
+        }
+    }
+    if web =="" or em=="":
+        messagebox.showerror('Oops!',"Please Don't Leave Any Field Empty!")
+    else:
+        is_ok = messagebox.askokcancel(title=web,message=f"These are the details entered:\nEmail:{em}\nPassword:{password}.\nIs it ok to save?")
+        if is_ok:
+        # file = open('data.txt','a')
+           # with open('data.txt','a') as file:
+            try:
+                with open('data.json','r') as file:
+                    # file.write(f"{data}\n")
+
+                    #Read Old Data
+                    data = json.load(file)
+
+            except FileNotFoundError:
+                with open('data.json','w') as file:
+                    #saving updated data
+                    json.dump(new_data,file,indent=4)          
+
+            else:
+                #Updating Data
+                data.update(new_data)
+                with open('data.json','w') as file:
+                    #saving updated data
+                    json.dump(data,file,indent=4)
+
+            finally: 
+                web_entry.delete(0,END)
+                password_entry.delete(0,END)
+
+            
+
+
+            msg = messagebox.showinfo('File Data',"Data Saved into 'Data.txt' file")
+
+def src_btn():
+    web = web_entry.get().capitalize()
+
+    with open('data.json','r') as file:
+        d = json.load(file)
+    
+    for i,j in d.items():
+        if i==web:
+            # print(f"{i}:{j}")
+            # for d in j:
+            # web_entry.insert(0,i)
+            # em_entry.delete(0,END)
+            # em_entry.insert(0,j['email'])
+            # password_entry.insert(0,j['password'])
+            messagebox.showinfo(i,f"Email: {'email'}\nPassword: {j['password']}")
+
+            break
+    else:
+        msg = messagebox.showerror('Search Data',"Data Not Found")
+
+def gen_pswd():
+    password = gen_pass()
+    password_entry.insert(0,password)
+
+
+search_btn = Button(root,text="Search",width=20,font=('Arial',10,'bold'),command=src_btn)
+search_btn.grid(row=1,column=2,sticky='w')
+gen_btn = Button(root,text="Generate Password",width=20,font=('Arial',10,'bold'),command=gen_pswd)
+gen_btn.grid(row=3,column=2,sticky='w')
+add_btn = Button(root,text="Add",width=89,font=('Arial',10,'bold'),command=add_data)
+add_btn.grid(row=4,column=1,columnspan=2,pady=2)
+
+
+root.mainloop()
